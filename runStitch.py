2914dcab677ce6a2eba5c8dcda073f9d79e9bdca
Riley Kopp <koppril7@gmail.com>
2019-11-25 18:53:55 -0700

    I need a break, your turn Jon

--- runStitch.py
+++ runStitch.py
@@ -12,21 +12,18 @@ if img1 is None or img2 is None:
 akaze = cv2.AKAZE_create()
 kpts1, desc1 = akaze.detectAndCompute(img1, None)
 kpts2, desc2 = akaze.detectAndCompute(img2, None)
-matcher = cv2.DescriptorMatcher_create(cv2.DescriptorMatcher_BRUTEFORCE_HAMMING)
-nn_matches = matcher.knnMatch(desc1, desc2, 2)
+matcher = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+nn_matches = matcher.match(desc1, desc2)
+nn_matches = sorted(nn_matches, key = lambda x:x.distance)
 matched1 = []
 matched2 = []
-nn_match_ratio = 0.8 # Nearest neighbor matching ratio
-for m, n in nn_matches:
-    if m.distance < nn_match_ratio * n.distance:
-        matched1.append(kpts1[m.queryIdx])
-        matched2.append(kpts2[m.trainIdx])
-matched1 = cv2.KeyPoint_convert(matched1)
-matched2 = cv2.KeyPoint_convert(matched2)
+for m in nn_matches:
+    matched1.append(kpts1[m.queryIdx].pt)
+    matched2.append(kpts2[m.trainIdx].pt)
+
+
 
 # End of openCV code
-
-
 
 #Our code:
 homography = ransac(matched1, matched2)
